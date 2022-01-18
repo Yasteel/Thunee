3978a87a091947867170d6e76f97ec4cf62f78fa
Yasteel Gungapursat <yasteel.gunga01@gmail.com>
2022-01-18 15:52:10 +0200

    added team file

---
+++ teams.js
@@ -0,0 +1,10 @@
+class Teams
+{
+  constructor(player_1, player_2, score, trumping)
+  {
+    this.player_1 = player_1;
+    this.player_2 = player_2;
+    this.score = score;
+    this.trumping = trumping;
+  }
+}
